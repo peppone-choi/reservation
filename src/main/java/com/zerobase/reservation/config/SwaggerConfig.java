40af41c16747c0f60c9fc26f5903c1dc189e001e
peppone-choi <peppone.choi@gmail.com>
2023-06-19 18:24:25 +0900

    base : Swagger를 통한 API 명세 생성

--- src/main/java/com/zerobase/reservation/config/SwaggerConfig.java
+++ src/main/java/com/zerobase/reservation/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
